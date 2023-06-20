4c9eed199bc492a2cf0ffb25e8f8ad46325a77e2
Mithilesh_cs <103479865+mitthhuu3110@users.noreply.github.com>
2023-06-20 14:06:24 +0530

    Create implementStackUsingArray.cpp

---
+++ Stacks And Queues/implementStackUsingArray.cpp
@@ -0,0 +1,26 @@
+class Stack {
+  int size;
+  int * arr;
+  int top;
+  public:
+    Stack() {
+      top = -1;
+      size = 1000;
+      arr = new int[size];
+    }
+  void push(int x) {
+    top++;
+    arr[top] = x;
+  }
+  int pop() {
+    int x = arr[top];
+    top--;
+    return x;
+  }
+  int Top() {
+    return arr[top];
+  }
+  int Size() {
+    return top + 1;
+  }
+};
