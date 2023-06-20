0fc249bf27a84100082498bbaaba7abb18124a32
Mithilesh_cs <103479865+mitthhuu3110@users.noreply.github.com>
2023-06-20 13:48:22 +0530

    Create palindromeLL.cpp

---
+++ linked list/palindromeLL.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+   bool isPalindrome(ListNode* head) {
+        ListNode* slow = head;
+        stack<int>s;
+        while(slow != NULL)
+        {
+            s.push(slow->val);
+            slow = slow->next;
+        }
+        while(head != NULL)
+        {
+            int i = s.top();
+            s.pop();
+            if(head->val != i) return false;
+            head = head->next;
+        }
+        return true;
+    }
+};
