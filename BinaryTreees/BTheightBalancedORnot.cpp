2187e7b9c78a6116df402d969fca3b23c43f22be
Mithilesh_cs <103479865+mitthhuu3110@users.noreply.github.com>
2023-06-21 22:51:42 +0530

    Create BTheightBalancedORnot.cpp

---
+++ BinaryTreees/BTheightBalancedORnot.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        return dfsHeight(root) != -1;
+    }
+
+    int dfsHeight(TreeNode* root){
+        if(root == NULL) return 0;
+        int left = dfsHeight(root -> left);
+        int right = dfsHeight(root -> right);
+        if(left == -1) return -1;
+        if(right == -1) return -1;
+
+        if(abs(left - right) > 1) return -1;
+        return max(left, right) + 1;
+    }
+};
