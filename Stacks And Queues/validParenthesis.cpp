c31f416b2012dbef7a7b7c283648e1558983ce23
Mithilesh_cs <103479865+mitthhuu3110@users.noreply.github.com>
2023-06-25 17:05:04 +0530

    Create validParenthesis.cpp

---
+++ Stacks And Queues/validParenthesis.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack <char> st;
+        int n=s.size();
+        
+        for(int i=0;i<n;i++){
+            if(st.empty())
+                st.push(s[i]);
+            
+            else if(st.top() == '(' && s[i] ==')')
+                st.pop();
+            
+            else if(st.top() == '{' && s[i] =='}')
+                st.pop();
+            
+            else if(st.top() == '[' && s[i] ==']')
+                st.pop();
+                
+            else
+                st.push(s[i]);
+        }
+        return st.empty();
+    }
+};
