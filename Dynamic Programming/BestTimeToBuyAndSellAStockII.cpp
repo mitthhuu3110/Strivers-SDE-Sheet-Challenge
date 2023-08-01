0b2298e1fd5411d845f944bfc78f27af39b00152
Mithilesh_cs <103479865+mitthhuu3110@users.noreply.github.com>
2023-08-02 00:32:02 +0530

    Update BestTimeToBuyAndSellAStockII.cpp

--- Dynamic Programming/BestTimeToBuyAndSellAStockII.cpp
+++ Dynamic Programming/BestTimeToBuyAndSellAStockII.cpp
@@ -1,1 +1,20 @@
+#include<bits/stdc++.h>
+long helper(int ind, int buy, long *values, int n, vector<vector<long>> &dp){
+    if(ind == n) return 0;
+    if(dp[ind][buy] != -1) return dp[ind][buy];
+    long profit = 0;
 
+    if(buy){
+        profit = max(-values[ind] + helper(ind+1, 0, values, n, dp), 0 + helper(ind+1, 1, values, n, dp));
+    }else{
+        profit = max(values[ind] + helper(ind+1, 1, values, n, dp), 0 + helper(ind+1, 0, values, n, dp));
+    }
+    return dp[ind][buy] = profit;
+}
+
+
+long getMaximumProfit(long *values, int n){
+    //Write your code here
+    vector<vector<long>> dp(n, vector<long>(2, -1));
+    return helper(0, 1, values, n, dp);
+}
