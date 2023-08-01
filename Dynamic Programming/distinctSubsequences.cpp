b1d685454457bf95637d97209ae8550f73040768
Mithilesh_cs <103479865+mitthhuu3110@users.noreply.github.com>
2023-08-01 23:50:42 +0530

    Create distinctSubsequences.cpp

---
+++ Dynamic Programming/distinctSubsequences.cpp
@@ -0,0 +1,21 @@
+class Solution {
+private:
+    int helper(int i, int j, string s, string t, vector<vector<int>> &dp){
+        if(j < 0) return 1;
+        if(i < 0) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(s[i] == t[j]){
+            return dp[i][j] = helper(i-1, j-1, s, t, dp) + helper(i-1, j, s, t, dp);
+        }
+        return dp[i][j] = helper(i-1, j, s, t, dp);
+    }
+public:
+    int numDistinct(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        vector<vector<int>> dp(n , vector<int>(m, -1));
+        return helper(n-1, m-1, s, t, dp);
+    }
+};
