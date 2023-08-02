b563c70cf19b37c16ced3316a1b0aa9e067f9a49
Mithilesh_cs <103479865+mitthhuu3110@users.noreply.github.com>
2023-08-02 12:35:20 +0530

    Create BestTimeToBuyAndSellAStockIV.cpp

---
+++ Dynamic Programming/BestTimeToBuyAndSellAStockIV.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+int maximumProfit(vector<int>& prices, int n, int k){
+    //Write your code here
+    vector<vector<int>> after(2, vector<int>(k+1, 0));
+    vector<vector<int>> curr(2, vector<int>(k+1, 0)); 
+    for(int ind = n-1; ind >= 0; ind--){
+        for(int buy = 0; buy <= 1; buy++){
+            for(int cap = 1; cap <= k; cap++){
+            if(buy == 1){
+                curr[buy][cap] = max(-prices[ind] + after[0][cap], 0 + after[1][cap]);
+            }else{
+                curr[buy][cap] = max(prices[ind] + after[1][cap-1], 0 + after[0][cap]);  
+            }
+            }
+        }
+        after = curr;
+    }
+    return after[1][k];
+}
+
+
+
