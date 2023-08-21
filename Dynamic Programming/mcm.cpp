d0b12957c78c19243867abfdac31d3f57b981d8f
Mithilesh_cs <103479865+mitthhuu3110@users.noreply.github.com>
2023-08-21 22:37:27 +0530

    Update mcm.cpp

--- Dynamic Programming/mcm.cpp
+++ Dynamic Programming/mcm.cpp
@@ -1,26 +1,19 @@
-int helper(vector<int> &arr,int i,int j, vector<vector<int>> &dp){
-    if(i == j){
-        return 0;
-    }
-    
-    if(dp[i][j]!=-1){
-        return dp[i][j];
-    }
-    
-    int steps = INT_MAX;
-    
-    for(int k = i;k < j;k++){
-        int one = helper(arr,i,k,dp);
-        int two = helper(arr,k+1,j,dp);
-        steps = min(steps,arr[i-1]*arr[k]*arr[j] + one + two);
-    }
-    
-    return dp[i][j] = steps;
-}
+#include <bits/stdc++.h> 
 
 int matrixMultiplication(vector<int> &arr, int N)
 {
-    int n = N;
-    vector<vector<int>> dp(n+1,vector<int> (n+1,-1));
-    return helper(arr,1,n-1,dp);
+    // Write your code here.
+    int dp[N][N];
+    for(int i = 1; i < N; i++) dp[i][i] = 0;
+    for(int i = N-1; i >= 1; i--){
+        for(int j = i+1; j < N; j++){
+            int mini = 1e9;
+            for(int k = i; k <= j-1; k++){
+                int steps = arr[i-1]*arr[k]*arr[j] + dp[i][k] + dp[k+1][j];
+                if(steps < mini) mini = steps;
+            }
+            dp[i][j] = mini;
+        }
+    }
+    return dp[1][N-1];
 }
