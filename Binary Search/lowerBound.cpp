3bbb5a7323e51c0aeb5e01bba9343a126bed2f08
Mithilesh_cs <103479865+mitthhuu3110@users.noreply.github.com>
2023-09-28 15:30:31 +0530

    Create lowerBound.cpp

---
+++ Binary Search/lowerBound.cpp
@@ -0,0 +1,14 @@
+int lowerBound(vector<int> arr, int n, int x) {
+	// Write your code here
+	int low = 0;
+	int high = n-1;
+	int res = n;
+	while(low <= high){
+		int mid = (low + high)/2;
+		if(arr[mid] >= x){
+			res = mid;
+			high = mid-1;
+		}else low = mid + 1;
+	}
+	return res;
+}
