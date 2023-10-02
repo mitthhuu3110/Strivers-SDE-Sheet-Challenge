123e0f064878692a46b3e5b3416aecc88f58ae03
Mithilesh_cs <103479865+mitthhuu3110@users.noreply.github.com>
2023-10-02 13:36:30 +0530

    Create singleElementInASortedArray.cpp

---
+++ Binary Search/singleElementInASortedArray.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+int singleNonDuplicate(vector<int>& arr){
+	// Write your code here
+	int n = arr.size();
+	if(arr[0] != arr[1]) return arr[0];
+	if(arr[n-1] != arr[n-2]) return arr[n-1];
+	int low = 1;
+	int high = n - 2;
+	while(low <= high){
+		int mid = (low + high)/2;
+		if(arr[mid] != arr[mid +1] && arr[mid] != arr[mid-1]) return arr[mid];
+		if((mid % 2 == 1 && arr[mid-1] == arr[mid]) || (mid%2 == 0 && arr[mid] == arr[mid+1])){
+			low = mid + 1;
+		}else high = mid - 1;
+	}
+	return -1;
+}
